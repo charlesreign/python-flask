589b1c6eec31d2f07561c542b95e41e37eb96c23
charlie <charlesgold45@gmail.com>
2022-02-11 10:33:35 +0000

    made changes to codebase

--- app.py
+++ app.py
@@ -1,25 +1,24 @@
 from os import abort
-from flask import Flask, session, redirect, url_for, render_template, request
+from flask import Flask, flash, session, redirect, url_for, render_template, request
 
 app = Flask(__name__)
+app.secret_key = "Drmhze6EPcv0fN_81Bj-nA"
 
 @app.route("/")
 def index():
-    return render_template('login.html')
+    return render_template('index.html')
 
 @app.route('/login', methods=['POST','GET'])
 def login():
+    error = None
     if request.method == 'POST':
-        if request.form['username'] == 'admin':
-            return redirect(url_for('success'))
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid username or password. Try again'
         else:
-            abort()
-    else:
-        return redirect(url_for('index'))
-
-@app.route('/success')
-def success():
-    return 'login successful'
+            flash('You lwere successfully logged in')
+            flash('logout before login')
+            return redirect(url_for('index'))
+    return render_template('login.html', error = error)
     
 
 if __name__ == "__main__":
