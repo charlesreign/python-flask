951b14d80f575544b94680157c16a7026b0e1725
charlie <charlesgold45@gmail.com>
2022-02-11 10:55:03 +0000

    wrote code to upload file

--- app.py
+++ app.py
@@ -1,24 +1,19 @@
 from os import abort
 from flask import Flask, flash, session, redirect, url_for, render_template, request
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.secret_key = "Drmhze6EPcv0fN_81Bj-nA"
 
-@app.route("/")
-def index():
+@app.route("/upload")
+def upload():
     return render_template('index.html')
 
-@app.route('/login', methods=['POST','GET'])
-def login():
-    error = None
+@app.route('/uploader', methods=['POST','GET'])
+def uploader():
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid username or password. Try again'
-        else:
-            flash('You lwere successfully logged in')
-            flash('logout before login')
-            return redirect(url_for('index'))
-    return render_template('login.html', error = error)
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        return 'file upload successful'
     
 
 if __name__ == "__main__":
